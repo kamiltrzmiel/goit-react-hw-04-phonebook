971956ddd497a3e9a0e9499f2bd7d4d8fe7c4d8d
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-06-29 22:52:47 +0200

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
     if (isMounted) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-  }, [contacts, isMounted]);
+  });
   
 
   const appendContacts = contact => {
